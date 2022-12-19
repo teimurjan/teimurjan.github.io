b9338a17cb0c48589a724aa988a6ff2d6dc561c9
teimurjan <teimurjan@users.noreply.github.com>
2022-12-19 12:08:19 +0000

    deploy: 13585635cc2fc8a46e930020628095b3361d102c

--- component---src-pages-404-tsx-74851369c7f321717b57.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_teimurjan_landing=self.webpackChunk_teimurjan_landing||[]).push([[218],{3503:function(n,l,t){t.r(l);var u=t(2146),e=t(6135),a=t(4129),r=t(9819);l.default=()=>(0,r.tZ)(u.Ar,null,(0,r.tZ)(a._g,null),(0,r.tZ)(u.VY,null,(0,r.tZ)(e.yX,{color:"green"},(0,r.tZ)(e.VM,{title:"404 - Not found"}))),(0,r.tZ)(a.$_,null))}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-74851369c7f321717b57.js.map

faf2a610ae67620998b05fdc82692e1917c259e1
Teimur Gasanov <teymurgg321@gmail.com>
2025-03-03 21:47:12 +0600

    Extract serverless functions into a separate workspace

--- workspaces/functions/api/deploy-webhook.ts
+++ workspaces/functions/api/deploy-webhook.ts
@@ -5,19 +5,29 @@ const HYGRAPH_SECRET = process.env.HYGRAPH_SECRET!
 const GITHUB_WORKFLOW_URL = process.env.GITHUB_WORKFLOW_URL!
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN!
 
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' })
   }
 
-  const signature = req.headers['x-hygraph-signature'] as string
-  const payload = JSON.stringify(req.body)
+  const signature = req.headers['gcms-signature'] as string
+  const [rawSign, rawEnv, rawTimestamp] = signature.split(', ')
 
-  const hmac = crypto.createHmac('sha256', HYGRAPH_SECRET)
-  hmac.update(payload)
-  const expectedSignature = `sha256=${hmac.digest('hex')}`
+  const sign = rawSign.replace('sign=', '')
+  const env = rawEnv.replace('env=', '')
+  const timestamp = parseInt(rawTimestamp.replace('t=', ''))
+  const payload = JSON.stringify({
+    Body: JSON.stringify(req.body),
+    EnvironmentName: env,
+    TimeStamp: timestamp,
+  })
+  const hash = crypto
+    .createHmac('sha256', HYGRAPH_SECRET)
+    .update(payload)
+    .digest('base64')
 
-  if (signature !== expectedSignature) {
+  if (sign !== hash) {
     return res.status(403).json({ message: 'Invalid signature' })
   }
 
@@ -26,9 +36,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
-        Accept: 'application/vnd.github.v3+json',
+        Accept: 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28',
       },
-      body: JSON.stringify({ event_type: 'content_updated' }),
+      body: JSON.stringify({ ref: 'main' }),
     })
 
     if (!response.ok) {

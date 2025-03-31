dbc9347219668870a7fdbd7a9b13ffb8383ccee4
Teimur Gasanov <teymurgg321@gmail.com>
2025-03-31 17:51:32 +0600

    Remove allowed origins

--- workspaces/resume-generator/next.config.ts
+++ workspaces/resume-generator/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['localhost:3000', 'a40e-91-205-48-121.ngrok-free.app'],
 };
 
 export default nextConfig;
